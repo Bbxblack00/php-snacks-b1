57a1863d6d879ab5a25c8831615da15a5b30eb08
Benito <bbxblack@gmail.com>
2021-05-04 21:02:28 +0200

    v0.2

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+
+    <style media="screen">
+
+    body{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      background-color: darkorange;
+
+    </style>
+    <title></title>
+  </head>
+  <body>
+      <?php
+
+      // Creiamo un array contenente le partite di
+      // basket di un'ipotetica tappa del calendario.
+      // Ogni array avrà una squadra di
+      // casa e una squadra ospite,
+      // punti fatti dalla squadra di casa e
+      // punti fatti dalla squadra ospite.
+      // Stampiamo a schermo tutte le
+      // partite con questo schema.
+
+      // Olimpia Milano - Cantù | 55-60
+
+      $index = [
+        'Milano' => [
+
+          'Teams' => [
+            'Milano',
+            'Roma'
+          ],
+          'score' => [
+            '27',
+            '33'
+          ]
+
+        ],
+
+        'Napoli' => [
+
+          'Teams' => [
+            'Napoli',
+            'Palermo'
+          ],
+          'score' => [
+            '2',
+            '3'
+          ]
+
+        ],
+
+        'Napoli' => [
+
+          'Teams' => [
+            'Napoli',
+            'Palermo'
+          ],
+          'score' => [
+            '2',
+            '3'
+          ]
+
+        ]
+      ];
+
+    ?>
+  </body>
+</html>
